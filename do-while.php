80b3cf21fcb843299f74ef099fd88d21b2c1cca0
Joshua Williams <joshuamw322@gmail.com>
2017-03-13 16:19:54 -0500

    included on examples of do while loops in exercise

--- do-while.php
+++ do-while.php
@@ -1,11 +1,29 @@
 <?php 
-$loop = 2;
+$loop1 = 0;
+$loop2 = 100;
+$loop3 = 2;
+
 
 do {
 
-	echo "\$loop is equal to {$loop}\n";
-	$loop *= $loop;
+	echo "\$loop is equal to {$loop1}\n";
+	$loop1++;
 
-} while ($loop < 1000000);
+} while ($loop1 <= 100);
+
+
+do {
+
+	echo "\$loop is equal to {$loop2}\n";
+	$loop2 -= 5;
+
+} while ($loop2 >= -10);
+
+do {
+
+	echo "\$loop is equal to {$loop3}\n";
+	$loop3 *= $loop3;
+
+} while ($loop3 < 1000000);
 
  ?>
