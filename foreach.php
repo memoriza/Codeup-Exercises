c0b15189a4fb08c30f68f8e6266693cef4e76dcf
Joshua Williams <joshuamw322@gmail.com>
2017-03-15 16:26:45 -0500

    foreach exercise first step structuring

---
+++ foreach.php
@@ -0,0 +1,15 @@
+<?php 
+
+$things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
+
+
+foreach ($things as $thing) {
+
+	if ($thing == "") {
+		echo "". PHP_EOL;
+	} else {
+		echo . PHP_EOL;
+	}
+}
+
+?>
