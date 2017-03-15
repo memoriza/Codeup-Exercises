a5d91b5e41ae63746cf9d3ad9a399146ffbb2b5a
Joshua Williams <joshuamw322@gmail.com>
2017-03-15 12:26:26 -0500

    changed order of if-else statements in order of specificity

--- for.php
+++ for.php
@@ -12,18 +12,17 @@ do {fwrite(STDOUT, "Enter in a numeric value for the end of the range: ");
 fwrite(STDOUT, "enter in a numeric increment value." . PHP_EOL);
 	$incr = trim(fgets(STDIN));
 
+if ($incr == false || $incr == 0 || $incr == "" || $incr == !is_numeric($incr)) { 
 
-if ($incr > 1) {
-
-	for ($i = $start; $i <= $end; $i+=$incr ) {
+	for ($i = $start; $i <= $end; $i++ ) {
 	
 		echo $i	. PHP_EOL;
 
 	}
 
-} elseif ($incr == false || $incr == 0 || $incr == "") { 
+} elseif ($incr >= 1) {
 
-	for ($i = $start; $i <= $end; $i++ ) {
+	for ($i = $start; $i <= $end; $i+=$incr ) {
 	
 		echo $i	. PHP_EOL;
 
