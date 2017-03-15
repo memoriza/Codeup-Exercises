74e816c5fb07927681b7b2265880117160117eea
Joshua Williams <joshuamw322@gmail.com>
2017-03-15 11:20:15 -0500

    Added user input for the incrementation value with do while functions to ensure that numeric values are input for the min and max value ranges.

--- for.php
+++ for.php
@@ -1,13 +1,40 @@
 <?php
 
-fwrite(STDOUT, "Enter the start of the range");
-$start = trim(fgets(STDIN));
 
-fwrite(STDOUT, "Enter the end of the range");
-$end = trim(fgets(STDIN));
+do {fwrite(STDOUT, "Enter in a numeric value for the start of the range: ");
+	$start = trim(fgets(STDIN));
+} while (!is_numeric($start));
 
-for ($i = $start; $i <= $end; $i++) {
-	if ($i % 2 === 0) {echo $i, PHP_EOL;
+do {fwrite(STDOUT, "Enter in a numeric value for the end of the range: ");
+	$end = trim(fgets(STDIN));
+} while (!is_numeric($end));
+
+fwrite(STDOUT, "enter in a numeric increment value." . PHP_EOL);
+	$incr = trim(fgets(STDIN));
+
+
+if ($incr > 1) {
+
+	for ($i = $start; $i <= $end; $i+=$incr ) {
+	
+		echo $i	. PHP_EOL;
+
+	}
+
+} elseif ($incr == false || $incr == 0 || $incr == "") { 
+
+	for ($i = $start; $i <= $end; $i++ ) {
+	
+		echo $i	. PHP_EOL;
+
+	}
+
+} else {
+
+	for ($i = $start; $i <= $end; $i++ ) {
+	
+		echo $i	. PHP_EOL;
+
 	}
 }
 
