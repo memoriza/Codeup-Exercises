e1fb28dc6bbe36463a68013091f68a9fde395adc
Joshua Williams <joshuamw322@gmail.com>
2017-03-16 13:56:51 -0500

    finished foreach book assignment and pulled finished version to master

---
+++ foreach_books.php
@@ -0,0 +1,44 @@
+<?php 
+
+$books = array(
+    'The Hobbit' => array(
+        'published' => 1937,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 310
+    ),
+    'Game of Thrones' => array(
+        'published' => 1996,
+        'author' => 'George R. R. Martin',
+        'pages' => 835
+    ),
+    'The Catcher in the Rye' => array(
+        'published' => 1951,
+        'author' => 'J. D. Salinger',
+        'pages' => 220
+    ),
+    'A Tale of Two Cities' => array(
+        'published' => 1859,
+        'author' => 'Charles Dickens',
+        'pages' => 544
+    )
+);
+
+foreach ($books as $title => $array) {
+
+
+	if ($array['published'] >= 1950) {
+		echo ($title) . PHP_EOL;
+		}
+
+	if ($array['published'] >= 1950) {
+
+		foreach ($array as $keytwo => $value) {
+		echo "$keytwo: $value" . PHP_EOL; 
+		}
+		
+	}
+
+}
+
+
+?>
