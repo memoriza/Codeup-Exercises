bed61778cbd142842d9bc448d3e2b66bbb455afe
Joshua Williams <joshuamw322@gmail.com>
2017-03-17 16:21:50 -0500

    added error messages for user inputs that are not numeric and for the division function if they are trying to divide by 0

--- arithmetic.php
+++ arithmetic.php
@@ -1,35 +1,58 @@
 <?php
 
-$a = 12;
-$b = 22;
 
-function add($a, $b)
-{
-    return $a + $b;
+function add($a, $b) {
+
+    if (is_numeric($a) == true || is_numeric($b) == true) {
+    	return $a + $b;
+	} else {
+		echo "Error! Input numeric values for function parameters not $a or $b";
+	}
 }
 
-function subtract($a, $b)
-{
-    return $a - $b;
+function subtract($a, $b) {
+
+	if (is_numeric($a) == true || is_numeric($b) == true) {
+    	return $a - $b;
+	} else {
+		echo "Error! Input numeric values for function parameters not $a or $b";
+	}
+    
 }
 
-function multiply($a, $b)
-{
-    return $a * $b;
+function multiply($a, $b) {
+
+	if (is_numeric($a) == true || is_numeric($b) == true) {
+    	return $a * $b;
+	} else {
+		return "Error! Input numeric values for function parameters not $a or $b";
+	}
 }
 
-function divide($a, $b)
-{
-    return $a / $b;
+function divide($a, $b) {
+
+    if (!is_numeric($a)  || !is_numeric($b) ) {
+    	return "Error! Input numeric values for function parameters not $a or $b";
+	} elseif ($b == 0) {
+		return "Error! Input non-zero numeric values as parameters";
+	} else {
+		return $a / $b;
+	}
 }
 
-function modulus($a, $b){
-	return $a % $b; 
+function modulus($a, $b) {
+
+	if (is_numeric($a) == true || is_numeric($b) == true) {
+    	return $a % $b;
+	} else {
+		return "Error! Input numeric values for function parameters not $a or $b";
+	}
 }
 
-echo add(3, 4) . PHP_EOL;
+echo add("farts", "poops") . PHP_EOL;
 echo subtract(45, 33) . PHP_EOL;
 echo multiply(2, 8) . PHP_EOL;
-echo divide(77, 7) . PHP_EOL;
-echo modulus(22, 2) . PHP_EOL;
+echo divide(7, "farts") . PHP_EOL;
+echo modulus(21, 2) . PHP_EOL;
 
+?>
